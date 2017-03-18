95430a6d9217f5613926b7e62c47186485dfe61d
Swapnil Soni <swap21296@gmail.com>
2017-03-18 22:03:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -25,6 +25,9 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+app.get('/favicon.ico', function (req, res) {
+  res.sendFile(path.join(__dirname, 'madi.png'));
+});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
