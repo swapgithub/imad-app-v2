b004600f88bdc07678e7b2a3a6a554f1bd6b6e75
Swapnil Soni <swap21296@gmail.com>
2017-03-25 18:12:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var config = {
     database: 'swapgithub',
     host: 'db.imad.hasura-app.io',
     port: '5432',
-    password: 'swapgithub',
+    password: 'db-swapgithub-98467',
 };
 
 var app = express();
